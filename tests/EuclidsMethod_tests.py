036722724ac1ba0a1c708aacbe2750275f63e5e1
Nick Otter <otternq@gmail.com>
2013-01-11 18:30:53 -0800

    added documentation to modules and tests

--- tests/EuclidsMethod_tests.py
+++ tests/EuclidsMethod_tests.py
@@ -1,17 +1,18 @@
 import unittest
-
 from GreatestCommonDenominator import EuclidsMethod
 
 
 class EuclidsMethodTest(unittest.TestCase):
-
+    """This will run unit tests on Euclids Method to make sure of its correctness"""
+    
     def testClassExample(self):
+        """checks that Eulids Method calculates the correct output (2) 
+        for the given inputs (64, 46)"""
         m = 64;
         n = 46;
         
-        o = EuclidsMethod.EuclidsMethod(64, 46)
-        self.assertEqual(o, 2)
+        output = EuclidsMethod.EuclidsMethod(64, 46)
+        self.assertEqual(output, 2)
         
-
 if __name__ == '__main__':
     unittest.main()
