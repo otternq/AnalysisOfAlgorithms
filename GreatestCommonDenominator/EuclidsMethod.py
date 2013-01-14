db3bde6863e007f8f5fb2d4ee3c3b95f20c43105
Nick Otter <otternq@gmail.com>
2013-01-14 12:40:54 -0800

    adding a test that was shown in class for Euclids method

--- GreatestCommonDenominator/EuclidsMethod.py
+++ GreatestCommonDenominator/EuclidsMethod.py
@@ -4,14 +4,21 @@ import sys;
 
 def EuclidsMethod(m,n):
     """Uses Euclids method to calculate the greatest common denominator"""
+    
+    #terminate recursion when n equals zero
     if n == 0:
         return m
+        
     return EuclidsMethod(n, m % n)
    
 #this will only run if this file is being executed from command line
 #and will be ignored of included by another scrips/module/project 
 if __name__ == '__main__':
+
+    #the first command line argument is considered m
     m = int(sys.argv[1])
+    
+    #the seconds command line argument is considered n
     n = int(sys.argv[2])
     
     print(EuclidsMethod(m, n))
--- tests/EuclidsMethod_tests.py
+++ tests/EuclidsMethod_tests.py
@@ -5,14 +5,19 @@ from GreatestCommonDenominator import EuclidsMethod
 class EuclidsMethodTest(unittest.TestCase):
     """This will run unit tests on Euclids Method to make sure of its correctness"""
     
-    def testClassExample(self):
+    def testEuclidsExample1(self):
         """checks that Eulids Method calculates the correct output (2) 
         for the given inputs (64, 46)"""
-        m = 64;
-        n = 46;
         
         output = EuclidsMethod.EuclidsMethod(64, 46)
         self.assertEqual(output, 2)
+        
+    def testEuclidsExample2(self):
+        """checks that Eulids Method calculates the correct output (2) 
+        for the given inputs (55, 22)"""
+        
+        output = EuclidsMethod.EuclidsMethod(55, 22)
+        self.assertEqual(output, 11)
         
 if __name__ == '__main__':
     unittest.main()
