d79a4a04a5627bc12d43e3bce603c6b12eae8dab
Nick Otter <otternq@gmail.com>
2013-01-14 13:12:56 -0800

    adding greatest common denominator function called EuclidsMinus although it needs renamed. tests to go with

--- GreatestCommonDenominator/EuclidsMethod.py
+++ GreatestCommonDenominator/EuclidsMethod.py
@@ -2,14 +2,24 @@ import sys;
 """A slightly modified version of the code provided in CS395 by Prof. Heckendorn on 1/11/2013"""
 
 
-def EuclidsMethod(m,n):
+def EuclidsMethodMod(m,n):
     """Uses Euclids method to calculate the greatest common denominator"""
     
     #terminate recursion when n equals zero
     if n == 0:
         return m
         
-    return EuclidsMethod(n, m % n)
+    return EuclidsMethodMod(n, m % n)
+    
+def EuclidsMethodMinus(m , n):
+
+    # if m is greater, swap variables
+    if m < n: (m, n) = (n, m)
+        
+    if n == 0:
+        return m
+        
+    return EuclidsMethodMinus(n, m-n)
    
 #this will only run if this file is being executed from command line
 #and will be ignored of included by another scrips/module/project 
@@ -21,4 +31,8 @@ if __name__ == '__main__':
     #the seconds command line argument is considered n
     n = int(sys.argv[2])
     
-    print(EuclidsMethod(m, n))
+    print "Euclids Method Mod: "
+    print EuclidsMethodMod(m, n)
+    
+    print "Euclids Method Minus: "
+    print EuclidsMethodMinus(m, n)
--- tests/EuclidsMethodMinus_tests.py
+++ tests/EuclidsMethodMinus_tests.py
@@ -2,35 +2,35 @@ import unittest
 from GreatestCommonDenominator import EuclidsMethod
 
 
-class EuclidsMethodModTest(unittest.TestCase):
+class EuclidsMethodMinusTest(unittest.TestCase):
     """This will run unit tests on Euclids Method to make sure of its correctness"""
     
-    def testEuclidsModExample1(self):
+    def testEuclidsMinusExample1(self):
         """checks that Eulids Method calculates the correct output (2) 
         for the given inputs (64, 46)"""
         
-        output = EuclidsMethod.EuclidsMethodMod(64, 46)
+        output = EuclidsMethod.EuclidsMethodMinus(64, 46)
         self.assertEqual(output, 2)
         
-    def testEuclidsModExample2(self):
+    def testEuclidsMinusExample2(self):
         """checks that Eulids Method calculates the correct output (55) 
         when the inputs are the same (55)"""
         
-        output = EuclidsMethod.EuclidsMethodMod(55, 55)
+        output = EuclidsMethod.EuclidsMethodMinus(55, 55)
         self.assertEqual(output, 55)
         
-    def testEuclidsModExample3(self):
+    def testEuclidsMinusExample3(self):
         """checks that Eulids Method calculates the correct output (11) 
         for the given inputs (55, 22)"""
         
-        output = EuclidsMethod.EuclidsMethodMod(55, 22)
+        output = EuclidsMethod.EuclidsMethodMinus(55, 22)
         self.assertEqual(output, 11)
         
-    def testEuclidsModExample4(self):
+    def testEuclidsMinusExample4(self):
         """checks that Eulids Method calculates the correct output (2) 
         for the given inputs (60, 14)"""
         
-        output = EuclidsMethod.EuclidsMethodMod(60, 14)
+        output = EuclidsMethod.EuclidsMethodMinus(60, 14)
         self.assertEqual(output, 2)
         
 if __name__ == '__main__':
